9f27f4b63272d49c24572a29f46b8e045b1d3e89
hwanghyun-lee <158053085+hwanghyun-lee@users.noreply.github.com>
2024-01-30 20:24:41 +0900

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
 import streamlit as st    
 
+st.title("파이썬API 시뮬레이션 프로그램")
+
+# Folium 지도 생성
+o_latitude = 37.362124	
+o_longitude = 127.114468
+
+m = folium.Map(location=[o_latitude,o_longitude], tiles="OpenStreetMap", zoom_start=13, size = 200, show = True)
+folium.TileLayer('cartodbdark_matter', name= 'Dark_Screen').add_to(m)
+
+
 
 st.write('# streamlit test-woowayouths')
 st.write('## Security Issues')
