ac31cca703a1e003ab9620dec646163d55a511fe
hwanghyun-lee <158053085+hwanghyun-lee@users.noreply.github.com>
2024-01-30 09:41:07 +0900

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import streamlit as st
+import streamlit 
 
-# import geopandas as gpd
-#import pandas as pd
-#import json
-# import folium
-# import folium.plugins as plug
-# import random
-#import numpy as np
-# import matplotlib.pyplot as plt
-# import urllib3
-# import requests
-# import time
-# import datetime as dt
-#import math
 
-# import folium_static
-# import Point, Polygon
-# import geodesic
-# import load_iris
 
 
 
