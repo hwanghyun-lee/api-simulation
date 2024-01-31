3195f3c8e3d21cc3db2c97081b079c1f0492da22
hwanghyun-lee <158053085+hwanghyun-lee@users.noreply.github.com>
2024-01-31 14:07:53 +0900

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 
 import streamlit as st    
-pip install folium
 import folium 
+import geopandas as gpd
+import streamlit as st
+import pandas as pd
+import json
+import folium
+import folium.plugins as plug
+import random
+import numpy as np
+
+import matplotlib.pyplot as plt
+import urllib3
+import requests
+import time
+import datetime as dt
+import math
+from folium import plugins
+
+
+from streamlit_folium import folium_static
+from shapely.geometry import Point, Polygon
+from geopy.distance import geodesic
+from sklearn.datasets import load_iris
+
 st.title("파이썬API 시뮬레이션 프로그램")
 
 # Folium 지도 생성
 o_latitude = 37.362124	
 o_longitude = 127.114468
 
-m = folium.Map(location=[o_latitude,o_longitude], tiles="OpenStreetMap", zoom_start=13, size = 200, show = True)
-folium.TileLayer('cartodbdark_matter', name= 'Dark_Screen').add_to(m)
+
 
 
 
