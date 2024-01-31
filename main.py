7abd55a032efb1bf255f2cbdc28440d095bdf738
hwanghyun-lee <158053085+hwanghyun-lee@users.noreply.github.com>
2024-01-31 20:44:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 
 import streamlit as st    
-import folium 
 import geopandas as gpd
-import streamlit as st
 import pandas as pd
-import json
-import folium
-import folium.plugins as plug
-import random
-import numpy as np
-
-import matplotlib.pyplot as plt
-import urllib3
-import requests
-import time
-import datetime as dt
-import math
-from folium import plugins
-
-
-from streamlit_folium import folium_static
-from shapely.geometry import Point, Polygon
-from geopy.distance import geodesic
-from sklearn.datasets import load_iris
-
-st.title("파이썬API 시뮬레이션 프로그램")
-
-# Folium 지도 생성
-o_latitude = 37.362124	
-o_longitude = 127.114468
-
-
-
-
 
 st.write('# streamlit test-woowayouths')
 st.write('## Security Issues')
