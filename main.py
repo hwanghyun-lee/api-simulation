d1141b0678666e02e373f468a50a33704db2ca97
hwanghyun-lee <158053085+hwanghyun-lee@users.noreply.github.com>
2024-02-01 19:47:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,28 @@ st.set_page_config(
 ### 전체 PPC 위/경도 파일 불러오기
 FC_url ="https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/Bmart_231112.csv"
 
+
+### PPC 권역그리기용 geojson
+gdf_test_url = "https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/bddong_geojson.geojson"
+
+data_dlvry_url = "https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/food.csv"
+data_dlvry2_url = "https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/bmart_boondang.csv"
+data_dlvry3_url = "https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/bmart_sungsam.csv"
+data_url = "https://raw.githubusercontent.com/hwanghyun-lee/api-simulation/main/bmart_boondang.csv"
+
+
+
+
+
+
 FC = pd.read_csv(FC_url)
+gdf_test = gpd.read_file(gdf_test_url)
+
+data_dlvry = pd.read_csv(data_dlvry_url)
+data_dlvry2 = pd.read_csv(data_dlvry2_url)
+data_dlvry3 = pd.read_csv(data_dlvry3_url)
+data = pd.read_csv(data_url)
+
 
 st.write(FC)
     
